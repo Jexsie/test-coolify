442ab735c9950dfc63900255ab354b62b3b2d111
Jessie Ssebuliba <jexsie@Jessies-MacBook-Air.local>
2025-05-13 15:10:36 +0200

    fix up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // important for external access
+    port: 3000,
+    allowedHosts: ["test-coolify.open-elements.cloud"], // ← add your domain here
+  },
+});
